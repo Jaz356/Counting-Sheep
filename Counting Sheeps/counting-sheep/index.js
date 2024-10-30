9d0fcf936c52c259823423f897c67251d14ff7ee
Jaz356 <joziahwalters70@gmail.com>
2024-10-30 11:54:54 +0100

    first commit

---
+++ Counting Sheeps/counting-sheep/index.js
@@ -0,0 +1,14 @@
+function countSheeps(list){
+    const count = list.filter(Boolean).length;
+
+    if (count !=0)
+        return "There are ${count} sheep in total";
+    if (count=0)
+        return "UPS!!! Wolfs eaten Sheeps";
+
+
+  const prueba1= [true,true,true,false,true,true,true,true,true,false,true,false,false,true,true]
+
+  console.log(test1);
+
+}
